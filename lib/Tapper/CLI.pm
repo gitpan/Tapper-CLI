8672f9bc8f76e2d46a9e6b6a3ebe03c573237781
AMD OSRC (AMD Operating System Research Center) <tapper@tapper-testing.org>
2011-03-02 22:57:24 +0000

    Import of AMD/Tapper-CLI-3.000001 from CPAN.
    gitpan-cpan-distribution: Tapper-CLI
    gitpan-cpan-version:      3.000001
    gitpan-cpan-path:         AMD/Tapper-CLI-3.000001.tar.gz
    gitpan-cpan-author:       AMD
    gitpan-cpan-maturity:     released

---
+++ lib/Tapper/CLI.pm
@@ -0,0 +1,34 @@
+package Tapper::CLI;
+
+use warnings;
+use strict;
+
+our $VERSION = '3.000001';
+
+=head1 NAME
+
+Tapper::CLI - Tapper - tapper-* commandline tools
+
+=head1 SYNOPSIS
+
+    use Tapper::CLI;
+    my $foo = Tapper::CLI->new();
+    ...
+
+=head1 AUTHOR
+
+AMD OSRC Tapper Team, C<< <tapper at amd64.org> >>
+
+=head1 BUGS
+
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright 2008-2011 AMD OSRC Tapper Team, all rights reserved.
+
+This program is released under the following license: freebsd
+
+
+=cut
+
+1; # End of Tapper::CLI
